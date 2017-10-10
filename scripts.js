8f29e59bf2b57c92adb1fa6cb8f42123c377b9ac
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-10-09 19:04:31 -0700

    Create views

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Backbone Model
 
-var Blog = Backbone.Model.extend({
+const Blog = Backbone.Model.extend({
   defaults: {
     author: '',
     title: '',
@@ -10,16 +10,16 @@ var Blog = Backbone.Model.extend({
 
 // Backbone Collection
 
-var Blogs = Backbone.Collection.extend({});
+const Blogs = Backbone.Collection.extend({});
 
 // Instantiate two Blogs
-var blog1 = new Blog({
+const blog1 = new Blog({
   author: 'Blanca',
   title: 'Blanca\'s Blog',
   url: 'http://blancasblog.com',
 });
 
-var blog2 = new Blog({
+const blog2 = new Blog({
   author: 'John',
   title: 'John\'s Blog',
   url: 'http://johnsblog.com',
@@ -27,4 +27,49 @@ var blog2 = new Blog({
 
 // Instantiate a Collection
 
-var blogs = new Blogs([blog1, blog2]);
+const blogs = new Blogs([blog1, blog2]);
+
+// Backbone view for one blog
+
+const BlogView = Backbone.View.extend({
+  model: new Blog(),
+  tagName: 'tr',
+  initialize() {
+    this.template = _.template($('.blogs-list-template').html());
+  },
+  render() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+// Bckbone view for all blogs
+
+const BlogsView = Backbone.View.extend({
+  model: blogs,
+  el: $('.blogs-list'),
+  initialize() {
+    this.model.on('add', this.render, this);
+  },
+  render() {
+    const self = this;
+    this.$el.html('');
+    _.each(this.model.toArray(), (blog) => {
+      self.$el.append((new BlogView({ model: blog })).render().$el);
+    });
+    return this;
+  }
+});
+
+const blogsView = new BlogsView();
+
+$(document).ready(() => {
+  $('.add-blog').on('click', () => {
+    const blog = new Blog({
+      author: $('.author-input').val(),
+      title: $('.title-input').val(),
+      url: $('.url-input').val(),
+    });
+    blogs.add(blog);
+  });
+});
